d3bda3c37aa9844b04491e8fb7fdabaeeb29505e
Jonas Ohland <jonas.ohland@gmail.com>
2020-07-13 01:48:15 +0200

    deploy-auto-commit @0.0.1 from jonasohland/spatial_intercom_server@539edc1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { PortTypes, PortTypeChannelCount, stringToPortType } from './dsp_defs';
 export { NodeAudioInputDescription, basicNodeAudioInputDescription } from './inputs_defs';
 export { UserData, basicUserData } from './users_defs';
-export {} from './web_interface_defs'
+export { nodeRoomName, serverRoomName, webifResponseEvent } from './web_interface_defs'
